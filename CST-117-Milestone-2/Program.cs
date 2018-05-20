1f2e212aa4094dc6f16580e258256b0f92c47909
Rihoj <rihoj1@gmail.com>
2018-05-20 16:49:28 -0400

    Created Inventory Class, Test Class, and main program

--- CST-117-Milestone-2/Program.cs
+++ CST-117-Milestone-2/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace CST_117_Milestone_2
 {
-    class Program
+    class Inventory
     {
         static void Main(string[] args)
         {
+            InventoryItem item = new InventoryItem("Chips", "A Bag of chips", 10, 2.99M, 0.5M);
+            Console.WriteLine("An item has been created.");
+            Console.WriteLine("Item Name: " + item.Name);
+            Console.WriteLine("Description: " + item.Description);
+            Console.WriteLine("Quantity: " + item.Quantity);
+            Console.WriteLine("Price: " + item.Price);
+            Console.WriteLine("Weight: " + item.Weight);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CST-117-Milestone-2Tests/InventoryItemTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CST_117_Milestone_2.Tests
+{
+    [TestClass()]
+    public class InventoryItemTests
+    {
+        private string name = "Test Item";
+        private string desc = "This is a test item";
+        private int quantity = 10;
+        private decimal weight = 15.534M;
+        private decimal price = 18.126M;
+
+        [TestMethod()]
+        public void InventoryItemTest()
+        {
+            InventoryItem item = new InventoryItem(name, desc, quantity, price, weight);
+            Assert.IsNotNull(item);
+            Assert.AreEqual(name, item.Name);
+            Assert.AreEqual(desc, item.Description);
+            Assert.AreEqual(quantity, item.Quantity);
+            Assert.AreEqual(18.13M, item.Price);
+            Assert.AreEqual(15.5M, item.Weight);
+        }
+
+        [TestMethod()]
+        public void purchaseItemTest()
+        {
+            InventoryItem item = new InventoryItem(name, desc, quantity, price, weight);
+            Assert.IsTrue(item.PurchaseItem(1));
+            Assert.IsFalse(item.PurchaseItem(10));
+        }
+
+        [TestMethod()]
+        public void OutOfStockTest()
+        {
+            InventoryItem item = new InventoryItem(name, desc, quantity, price, weight);
+            Assert.IsFalse(item.OutOfStock());
+            item.PurchaseItem(10);
+            Assert.IsTrue(item.OutOfStock());
+        }
+    }
+}
